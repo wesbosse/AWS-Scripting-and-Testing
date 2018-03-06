2764fa7508824a1bb79b611d696d8af3c1528801
wesbosse <wes.bosse@gmail.com>
2018-03-06 11:46:14 -0800

    cleaning up unused files

--- aws-ssh.sh
+++ aws-ssh.sh
@@ -2,6 +2,7 @@
 # Connect to AWS EC2 Instance
 echo -e "Please enter your public DNS: "
 read dns
-ssh -i testInstancekeypair.pem ec2-user@$dns 'bash -s' < ec2-update.sh
 ssh -i testInstancekeypair.pem ec2-user@$dns
+# ssh -i testInstancekeypair.pem ec2-user@$dns 'bash -s' < ec2-update.sh
+# ssh -i testInstancekeypair.pem ec2-user@$dns
 
